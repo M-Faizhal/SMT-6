9df028887fc6a52a0d0dd5eb356aece7345b6739
M-Faizhal <Faizhalmusthafa@gmail.com>
2022-02-09 23:03:39 +0700

    Update ProductController.php

--- EComm-en/app/Http/Controllers/ProductController.php
+++ EComm-en/app/Http/Controllers/ProductController.php
@@ -4,17 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\Product;
-use App\Models\Cart;
-use App\Models\Order;
 
 
 class ProductController extends Controller
 {
     function index()
     {
-        // $data= Product::all();
-
-        // return view('product',['products'=>$data]);
+        
     }
 }
